4142b52ab42170db6c52f0e803504f80e8a2583d
yabuchichi <167418350+yabuchichi@users.noreply.github.com>
2025-06-27 12:09:03 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -23,9 +23,9 @@ public class DiceGame{
 
     
     if(total >= 8){
-      System.out.println("You won !");
+      System.out.println(name + " won !");
     }else{
-      System.out.println("You lost !");
+      System.out.println(name + " lost !");
     }
     scanner.close();
   }
